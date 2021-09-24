530f0ab3a3bdf1b18a6c8416ad5641ac227e31a2
Iaroslav Sorokin <iaroslav.sorokin@gmail.com>
2021-09-24 13:14:25 +0300

    Add: client/server division.

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,25 @@ use std::alloc::handle_alloc_error;
 use std::thread;
 use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
+use std::env;
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if let Some(arg) = args.get(1) {
+        match arg.as_str() {
+            "serve" => run_as_server(args),
+            "connect" => run_as_client(args),
+            _ => ()
+        }
+    }
+}
+
+fn run_as_client(args: Vec<String>) {
+
+}
+
+fn run_as_server(args: Vec<String>) {
     let listener = TcpListener::bind("0.0.0.0:7878").unwrap();
 
     for stream in listener.incoming() {
