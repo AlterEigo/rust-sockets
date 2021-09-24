878a42492ee73d36f0e105897139baf0de3cd2a6
Iaroslav Sorokin <iaroslav.sorokin@gmail.com>
2021-09-24 12:52:47 +0300

    Add: spawning new threads for new connections.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,25 @@
-use std::net::TcpListener;
+use std::alloc::handle_alloc_error;
+use std::thread;
+use std::io::prelude::*;
+use std::net::{TcpListener, TcpStream};
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let listener = TcpListener::bind("0.0.0.0:7878").unwrap();
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
         println!("[Server] : New connection established.");
+        thread::spawn(|| {
+            handle_connection(stream);
+        });
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+
+    stream.read(&mut buffer).unwrap();
+
+    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 }
